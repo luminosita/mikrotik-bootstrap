05c84c622304161720888d1d40f2f9db399f8b52
luminosita <milosh@emisia.net>
2024-10-14 18:57:37 +0200

    rename certificates

--- tf/finalize.sh
+++ tf/finalize.sh
@@ -11,8 +11,8 @@ ssh $TF_VAR_adminuser@$TF_VAR_host -p 2222 $DELETE_SETUP_SCRIPT
 mkdir ../certs
 
 scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/cert_export_MikroTik.crt ../certs/ca.crt
-scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/cert_export_chat-server@MikroTik.crt ../certs/server.crt
-scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/cert_export_chat-server@MikroTik.key ../certs/server.key
+scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/cert_export_chat-server@MikroTik.crt ../certs/client.crt
+scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/cert_export_chat-server@MikroTik.key ../certs/client.key
 scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/server-pass.txt ../certs/
 scp -P 2222 $TF_VAR_adminuser@$TF_VAR_host:/passphrase.txt ../certs/
 
